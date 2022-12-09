8025d296afcc350692ef2f13452e382facb7f4f3
narumincho <16481886+narumincho@users.noreply.github.com>
2022-12-09 12:03:06 +0900

    update README.md

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Deno で https から import しても動くモジュールにする
 
+https://zenn.dev/narumincho/articles/231e1e5347fa9d
+
+`./deno.png` を `./deno.png.json` に変換する
+
 ```ps1
-deno run --allow-read=./deno.png --allow-write=./deno.png.json .\embedDenoPng.ts
+deno run --allow-read=./deno.png --allow-write=./deno.png.json ./embedDenoPng.ts
 ```
 
 動作確認 実行
--- start.ts
+++ start.ts
@@ -1,3 +1,3 @@
-import { startDenoImageServer } from "https://raw.githubusercontent.com/narumincho/deno-https-import-test/7c9a2c1caed59fcec84a794f266f23a59b84b111/imageServerUseReadFile.ts";
+import { startDenoImageServer } from "https://raw.githubusercontent.com/narumincho/deno-https-import-test/83d95134eafb394556f3b39de6255a7caf0bcd2a/imageServerUseJsonImport.ts";
 
 startDenoImageServer();
